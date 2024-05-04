b202aadbd8b771de83773e146891f6853c941b27
Brandon Gonzalez <brajgon@gmail.com>
2024-05-04 16:45:48 -0600

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// API Key e1a76ee6
+
+const API_URL = 'http://www.omdbapi.com?apikey=e1a76ee6'
 
 const App = () => {
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    console.log(data.Search);
+}
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []);
+
     return (
         <div>
             <h1>App</h1>
